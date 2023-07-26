f01ab0e302fa8c1f0e8397369d65f678372dfcdc
christopherflores9312 <christopherflores9312@gmail.com>
2023-07-25 22:07:57 -0700

    modded action.js

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -5,14 +5,15 @@ import { QUERY_CHECKOUT } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import CartItem from '../CartItem';
 import Auth from '../../utils/auth';
-import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleCart, addMultipleToCart } from '../../utils/actions';
 import './style.css';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state); // Access state
+  const dispatch = useDispatch(); // Get dispatch function
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
@@ -26,17 +27,13 @@ const Cart = () => {
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise('cart', 'get');
-      dispatch({ type: ADD_MULTIPLE_TO_CART, products: [...cart] });
+      dispatch(addMultipleToCart([...cart])); // Dispatch an action
     }
 
     if (!state.cart.length) {
       getCart();
     }
   }, [state.cart.length, dispatch]);
-
-  function toggleCart() {
-    dispatch({ type: TOGGLE_CART });
-  }
 
   function calculateTotal() {
     let sum = 0;
@@ -62,7 +59,7 @@ const Cart = () => {
 
   if (!state.cartOpen) {
     return (
-      <div className="cart-closed" onClick={toggleCart}>
+      <div className="cart-closed" onClick={() => dispatch(toggleCart())}> // Dispatch an action
         <span role="img" aria-label="trash">
           🛒
         </span>
--- client/src/utils/actions.js
+++ client/src/utils/actions.js
@@ -1,11 +1,71 @@
 export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
-
 export const ADD_TO_CART = "ADD_TO_CART";
 export const ADD_MULTIPLE_TO_CART = "ADD_MULTIPLE_TO_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
 export const CLEAR_CART = "CLEAR_CART";
 export const UPDATE_CART_QUANTITY = "UPDATE_CART_QUANTITY";
 export const TOGGLE_CART = "TOGGLE_CART";
-
 export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
 export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
+
+export const updateProducts = (products) => {
+  return {
+    type: UPDATE_PRODUCTS,
+    products
+  };
+};
+
+export const addToCart = (product) => {
+  return {
+    type: ADD_TO_CART,
+    product
+  };
+};
+
+export const addMultipleToCart = (products) => {
+  return {
+    type: ADD_MULTIPLE_TO_CART,
+    products
+  };
+};
+
+export const removeFromCart = (_id) => {
+  return {
+    type: REMOVE_FROM_CART,
+    _id
+  };
+};
+
+export const clearCart = () => {
+  return {
+    type: CLEAR_CART
+  };
+};
+
+export const updateCartQuantity = (_id, purchaseQuantity) => {
+  return {
+    type: UPDATE_CART_QUANTITY,
+    _id,
+    purchaseQuantity
+  };
+};
+
+export const toggleCart = () => {
+  return {
+    type: TOGGLE_CART
+  };
+};
+
+export const updateCategories = (categories) => {
+  return {
+    type: UPDATE_CATEGORIES,
+    categories
+  };
+};
+
+export const updateCurrentCategory = (currentCategory) => {
+  return {
+    type: UPDATE_CURRENT_CATEGORY,
+    currentCategory
+  };
+};
